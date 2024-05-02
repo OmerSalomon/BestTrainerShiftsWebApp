db6fa3a0af1e39564fa5470856d4cb54cd84aec3
OmerSalomon <102198073+OmerSalomon@users.noreply.github.com>
2024-05-02 03:48:18 +0300

    setup algorithm

--- src/main/java/entities/algorithm/Constants.java
+++ src/main/java/entities/algorithm/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final int SHIFTS_PER_WEEK = DAYS_IN_WEEK * SHIFTS_PER_DAY;
     public static final double SHIFT_WITHOUT_MANAGER_PENALTY = 5;
     public static final double TRAINER_UNDER_BOOK_PENALTY = 5;
-    public static final double NOT_ENOUGH_REST_PENALTY = 4;
+    public static final double NOT_ENOUGH_REST_PENALTY = 1;
 }
